5114cf45c6036db2d77e7f03de69a15839cabaed
Alexander Gude <gude@physics.umn.edu>
2014-04-17 13:56:20 -0500

    Adding Version 0,1 to CaloTPGTranscoderULUT
    Version 0 uses the old segmentation (<= 2012), while Version 1 uses the
    full segmented HF (> 2012).
    
    This has necessitated fixing calls to HTvalid and getOutputLUTId in
    other pieces of code. For cases where a HcalTrigTowerDetId was not
    available (and hence we could not use its internal version variable), we
    have hard coded the version to 0.
    
    Remove unused hcaletValue functions
    
    Removed from CaloTPGTranscoderULUT and CaloTPGTranscoder.

--- CalibCalorimetry/CaloTPG/src/CaloTPGTranscoderULUT.h
+++ CalibCalorimetry/CaloTPG/src/CaloTPGTranscoderULUT.h
@@ -10,7 +10,7 @@
 
 
 /** \class CaloTPGTranscoderULUT
-  *  
+  *
   * \author J. Mans - Minnesota
   */
 
@@ -25,18 +25,16 @@ public:
   virtual EcalTriggerPrimitiveSample ecalCompress(const EcalTrigTowerDetId& id, unsigned int sample, bool fineGrain) const;
 
   virtual void rctEGammaUncompress(const HcalTrigTowerDetId& hid, const HcalTriggerPrimitiveSample& hc,
-				   const EcalTrigTowerDetId& eid, const EcalTriggerPrimitiveSample& ec, 
+				   const EcalTrigTowerDetId& eid, const EcalTriggerPrimitiveSample& ec,
 				   unsigned int& et, bool& egVecto, bool& activity) const;
   virtual void rctJetUncompress(const HcalTrigTowerDetId& hid, const HcalTriggerPrimitiveSample& hc,
-				   const EcalTrigTowerDetId& eid, const EcalTriggerPrimitiveSample& ec, 
+				   const EcalTrigTowerDetId& eid, const EcalTriggerPrimitiveSample& ec,
 				   unsigned int& et) const;
-  virtual double hcaletValue(const int& ieta, const int& compressedValue) const;
-  virtual double hcaletValue(const int& ieta, const int& iphi, const int& compressedValue) const;
   virtual double hcaletValue(const HcalTrigTowerDetId& hid, const HcalTriggerPrimitiveSample& hc) const;
-  virtual bool HTvalid(const int ieta, const int iphi) const;
+  bool HTvalid(const int ieta, const int iphi, const int version) const;
   virtual std::vector<unsigned char> getCompressionLUT(HcalTrigTowerDetId id) const;
   virtual void setup(HcalLutMetadata const&, HcalTrigTowerGeometry const&);
-  virtual int getOutputLUTId(const int ieta, const int iphi) const;
+  int getOutputLUTId(const int ieta, const int iphi, const int version) const;
 
  private:
   // Typedef
@@ -45,7 +43,11 @@ public:
 
   // Constant
   // TODO prefix k
-  static const int NOUTLUTS = 4176;
+  // Version 0 LUTs: 72 phis * 28 towers for HB/HE, 18*4 for HF
+  static const int NUM_V0_LUTS = 2 * (72*28 + 18*4);
+  // Version 1 LUTs: 72 phis * 2 for split 28,29 in HE, full granularity in HF
+  static const int NUM_V1_LUTS = 2 * (72*2 + 36*12);
+  static const int NOUTLUTS = NUM_V0_LUTS + NUM_V1_LUTS;
   static const unsigned int OUTPUT_LUT_SIZE = 1024;
   static const int TPGMAX = 256;
   static const bool newHFphi = true;
