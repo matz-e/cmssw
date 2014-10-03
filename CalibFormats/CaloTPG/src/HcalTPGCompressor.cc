42fbee9cf99776b4144b0ea084b69eec8ffca483
Matthias Wolf <matthias.wolf@cern.ch>
2014-10-03 14:25:11 +0200

    Allow to compress samples in HCAL upgrade TP.

--- CalibFormats/CaloTPG/src/HcalTPGCompressor.cc
+++ CalibFormats/CaloTPG/src/HcalTPGCompressor.cc
@@ -3,12 +3,19 @@
 
 HcalTPGCompressor::HcalTPGCompressor(const CaloTPGTranscoder* coder) : coder_(coder) {
 }
-  
+
 void HcalTPGCompressor::compress(const IntegerCaloSamples& ics, const std::vector<bool>& fineGrain, HcalTriggerPrimitiveDigi& digi) const {
   digi.setSize(ics.size());
   digi.setPresamples(ics.presamples());
   for (int i=0; i<ics.size(); i++)
     digi.setSample(i,coder_->hcalCompress(ics.id(),ics[i],fineGrain[i]));
+}
+
+void HcalTPGCompressor::compress(const IntegerCaloSamples& ics, const std::vector<bool>& fineGrain, HcalUpgradeTriggerPrimitiveDigi& digi) const {
+  digi.setSize(ics.size());
+  digi.setPresamples(ics.presamples());
+  for (int i=0; i<ics.size(); i++)
+    digi.setSample(i,coder_->hcalUpgradeCompress(ics.id(),ics[i],fineGrain[i]));
 }
 
 HcalTriggerPrimitiveSample HcalTPGCompressor::compress(const HcalTrigTowerDetId& id, unsigned int sample, bool fineGrain) const {
