2abff6dcbefe6a2108139afa04e223e7ce8f8b43
Matthias Wolf <matthias.wolf@cern.ch>
2014-12-18 16:02:13 +0100

    [tmp] Update testing configs.

--- SimCalorimetry/HcalTrigPrimAlgos/test/run_mixing.py
+++ SimCalorimetry/HcalTrigPrimAlgos/test/run_mixing.py
@@ -5,14 +5,14 @@
 # with command line options: step2 --conditions W19_500_62E2::All -n 10 --eventcontent FEVTDEBUGHLT -s DIGI:pdigi_valid,L1,DIGI2RAW --datatier GEN-SIM-DIGI-RAW --customise SLHCUpgradeSimulations/Configuration/combinedCustoms.cust_2019,SLHCUpgradeSimulations/Configuration/aging.customise_aging_500 --geometry Extended2019 --magField 38T_PostLS1 --filein /store/relval/CMSSW_6_2_0_SLHC17/RelValTTbar_14TeV/GEN-SIM-DIGI-RAW/DES19_62_V8_UPG2019withGEM-v1/00000/000399A6-4A2D-E411-8A32-0025905A60AA.root --fileout step2.root
 import FWCore.ParameterSet.Config as cms
 
-process = cms.Process('TPPROD')
+process = cms.Process('MIX')
 
 # import of standard configurations
 process.load('Configuration.StandardSequences.Services_cff')
 # process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
 process.load('FWCore.MessageService.MessageLogger_cfi')
 process.load('Configuration.EventContent.EventContent_cff')
-process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('SimGeneral.MixingModule.mix_E14TeV_L28E32_BX2808_cfi')
 process.load('Configuration.Geometry.GeometryExtended2019Reco_cff')
 process.load('Configuration.StandardSequences.MagneticField_38T_PostLS1_cff')
 process.load('Configuration.StandardSequences.Digi_cff')
@@ -34,20 +34,13 @@ process.options = cms.untracked.PSet(
 
 )
 
-# Production Info
-process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.20 $'),
-    annotation = cms.untracked.string('step2 nevts:10'),
-    name = cms.untracked.string('Applications')
-)
-
 # Output definition
 
 process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
     splitLevel = cms.untracked.int32(0),
     eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
     outputCommands = process.FEVTDEBUGHLTEventContent.outputCommands,
-    fileName = cms.untracked.string('step2.root'),
+    fileName = cms.untracked.string('mixed_relval.root'),
     dataset = cms.untracked.PSet(
         filterName = cms.untracked.string(''),
         dataTier = cms.untracked.string('DIGI-RECO')
@@ -60,6 +53,16 @@ process.FEVTDEBUGHLToutput = cms.OutputModule("PoolOutputModule",
 # process.mix.digitizers = cms.PSet(process.theDigitizersValid)
 from Configuration.AlCa.GlobalTag import GlobalTag
 process.GlobalTag = GlobalTag(process.GlobalTag, 'auto:upgrade2019', '')
+
+process.mix.input.fileNames = cms.untracked.vstring([
+#     '/store/relval/CMSSW_5_2_1/RelValMinBias/GEN-SIM/START52_V4-v1/0003/4C958749-9872-E111-A747-003048F1183E.root',
+#     '/store/relval/CMSSW_5_2_1/RelValMinBias/GEN-SIM/START52_V4-v1/0002/5A081FCB-6772-E111-9623-0025B3244166.root'
+    '/store/relval/CMSSW_6_2_0_SLHC17/RelValMinBias_TuneZ2star_14TeV/GEN-SIM-DIGI-RAW/DES19_62_V8_UPG2019withGEM-v1/00000/46FCAEF6-452D-E411-953F-0025905A6094.root',
+    '/store/relval/CMSSW_6_2_0_SLHC17/RelValMinBias_TuneZ2star_14TeV/GEN-SIM-DIGI-RAW/DES19_62_V8_UPG2019withGEM-v1/00000/6A915485-442D-E411-ADB2-0025905A60D0.root',
+    '/store/relval/CMSSW_6_2_0_SLHC17/RelValMinBias_TuneZ2star_14TeV/GEN-SIM-DIGI-RAW/DES19_62_V8_UPG2019withGEM-v1/00000/72064111-3B2D-E411-8ADE-0025905B860E.root'
+])
+
+process.mix.input.nbPileupEvents = cms.PSet(averageNumber = cms.double(25.0))
 
 # Path and EndPath definitions
 process.digitisation_step = cms.Path(process.pdigi_valid)
@@ -83,10 +86,10 @@ process.poke = cms.Path()
 #         fileName = cms.string("debug.root"))
 
 # Schedule definition
-# process.schedule = cms.Schedule(process.digitisation_step,process.L1simulation_step,process.digi2raw_step,process.endjob_step,process.FEVTDEBUGHLToutput_step)
+process.schedule = cms.Schedule(process.digitisation_step,process.L1simulation_step,process.digi2raw_step,process.endjob_step,process.FEVTDEBUGHLToutput_step)
 # process.simpledigi = cms.Path(process.simHcalTriggerPrimitiveDigis)
 # process.schedule = cms.Schedule(process.simpledigi,process.poke,process.endjob_step,process.FEVTDEBUGHLToutput_step)
-process.schedule = cms.Schedule(process.digitisation_step,process.poke,process.endjob_step,process.FEVTDEBUGHLToutput_step)
+# process.schedule = cms.Schedule(process.digitisation_step,process.poke,process.endjob_step,process.FEVTDEBUGHLToutput_step)
 # process.schedule = cms.Schedule(process.simpledigi,process.endjob_step,process.FEVTDEBUGHLToutput_step)
 
 # customisation of the process.
@@ -98,8 +101,8 @@ from SLHCUpgradeSimulations.Configuration.combinedCustoms import cust_2019
 process = cust_2019(process)
 
 process.simHcalTriggerPrimitiveDigis.inputLabel = cms.VInputTag(
-        cms.InputTag("simHcalUnsuppressedDigis","HBHEUpgradeDigiCollection","DIGI2RAW"),
-        cms.InputTag("simHcalUnsuppressedDigis","HFUpgradeDigiCollection","DIGI2RAW"))
+        cms.InputTag("simHcalUnsuppressedDigis","HBHEUpgradeDigiCollection","MIX"),
+        cms.InputTag("simHcalUnsuppressedDigis","HFUpgradeDigiCollection","MIX"))
 
 # Automatic addition of the customisation function from SLHCUpgradeSimulations.Configuration.aging
 # from SLHCUpgradeSimulations.Configuration.aging import customise_aging_500 
@@ -109,4 +112,5 @@ process.simHcalTriggerPrimitiveDigis.inputLabel = cms.VInputTag(
 
 # End of customisation functions
 
-# print process.dumpPython()
+with open('dumped_config.py', 'w') as f:
+    f.write(process.dumpPython())
